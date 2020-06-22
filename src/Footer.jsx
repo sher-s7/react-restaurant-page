cb29b402eea0f0d2a164eb7ae6f4470ec09a44a9
Sher <sher.sheikh77@gmail.com>
2020-06-22 16:16:22 -0400

    Fix footer positioning

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,6 +4,8 @@ import Home from './Home'
 // import About from './About'
 // import Contact from './Contact'
 
+import GithubLogo from './images/github-logos/GitHub-Mark-Light-120px-plus.png'
+
 export default class Footer extends React.Component {
     handleClick(page) {
         
@@ -11,13 +13,19 @@ export default class Footer extends React.Component {
     render() {
         return (
             <footer>
-                <h3>TEATIME</h3>
                 <ul>
-                    <li><button onClick={() => this.handleClick('home')}>Home</button></li>
-                    <li><button onClick={() => this.handleClick('menu')}>Menu</button></li>
-                    <li><button onClick={() => this.handleClick('about')}>About</button></li>
-                    <li><button onClick={() => this.handleClick('contact')}>Contact</button></li>
+                    <li><h3>TEATIME</h3></li>
+                    <li><span onClick={() => this.handleClick('home')}>Home</span></li>
+                    <li><span onClick={() => this.handleClick('menu')}>Menu</span></li>
+                    <li><span onClick={() => this.handleClick('about')}>About</span></li>
+                    <li><span onClick={() => this.handleClick('contact')}>Contact</span></li>
                 </ul>
+                <div className='createdby'>
+                    <span>Created by Sher Sheikh</span>
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/sher-s7/react-restaurant-page">
+                        <img id='github-logo' src={GithubLogo} alt="github-logo"/>
+                    </a>
+                </div>
             </footer>
         );
     }
--- src/TabContentContainer.jsx
+++ src/TabContentContainer.jsx
@@ -34,19 +34,19 @@ export default class TabContentContainer extends React.Component {
                 <ul id='tablist'>
                     <li className='tab'
                     id={this.state.currentTab === 0 ? 'current-tab' : null} 
-                    onClick={() => this.handleClick('home')}><h2>Home</h2></li>
+                    onClick={() => this.handleClick('home')}><h2>HOME</h2></li>
                     <li className='tab'
                     id={this.state.currentTab === 1 ? 'current-tab' : null}
-                    onClick={() => this.handleClick('menu')}><h2>Menu</h2></li>
+                    onClick={() => this.handleClick('menu')}><h2>MENU</h2></li>
                     <li className='tab'
                     id={this.state.currentTab === 2 ? 'current-tab' : null}
-                    onClick={() => this.handleClick('about')}><h2>About</h2></li>
+                    onClick={() => this.handleClick('about')}><h2>ABOUT</h2></li>
                     <li className='tab'
                     id={this.state.currentTab === 3 ? 'current-tab' : null}
-                    onClick={() => this.handleClick('contact')}><h2>Contact</h2></li>
+                    onClick={() => this.handleClick('contact')}><h2>CONTACT</h2></li>
                 </ul>
                 
-                {this.state.content}
+                <div className='contentContainer'>{this.state.content}</div>
             </div>
         );
     }
