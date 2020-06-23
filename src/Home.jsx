f22fddba162d3de012706103ece79e2180ef4a2f
Sher <sher.sheikh77@gmail.com>
2020-06-22 20:34:35 -0400

    Finish home page and add favicon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import TeaPot from './images/teacup-leaf.png'
 
 export default class Home extends React.Component {
     render() {
         return (
             <div id='home'>
-                <p id='home-message'>Enjoy a freshly brewed cup of tea anytime</p>
+                <blockquote>
+                    <p>There is something in the nature of tea that leads us into a world of quiet contemplation of life.</p>
+                    <p className='citation'>—Lin Yutang, <cite>The Importance of Living</cite></p>
+                </blockquote>
+
+                <div className='parallax-container'><div className='parallax' id='teabanner' alt="loose-leaf-tea" /></div>
+
+
+                <p id='home-message'>TEATIME provides a relaxing, welcoming atmosphere
+                with with organic loose leaf teas, tasty appetizers, and friendly service.
+                    Your go to place to unwind, and enjoy a freshly brewed cup of tea. </p>
+                <img id='teacup-leaf' src={TeaPot} alt="teapot.png" />
             </div>
         );
     }
